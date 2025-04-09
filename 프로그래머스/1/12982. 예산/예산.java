a586c700138d695e46b4deb10e8f93ec1d949a3a
beomseok90 <42230360+beomseok90@users.noreply.github.com>
2025-04-09 17:49:50 +0900

    [level 1] Title: 예산, Time: 0.53 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int count = 0;
+        int sum = 0;
+        
+        Arrays.sort(d);
+        
+        for (int i = 0; i < d.length; i++) {
+            if (sum + d[i] > budget) {
+                break;
+            }
+            sum += d[i];
+            count++;
+        }
+        
+        return count;
+    }
+}
